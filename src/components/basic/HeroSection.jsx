097fda811ab71426b4a292a336d9087293c7049a
hasanbut17314 <hasanbut17314@gmail.com>
2025-02-04 15:35:59 +0500

    initialize React app with Vite, Tailwind CSS, and basic layout components

---
+++ src/components/basic/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='m-20'>
+            {/* <button className='btn'>Hello</button> */}
+            Hero Section
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import HeroSection from '../basic/HeroSection'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar />
+            <HeroSection />
+        </header>
+    )
+}
+
+export default Header
